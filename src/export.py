ebaa3f2bb48bf98c58e289e38f51bf6b1c9636f9
Brons1981 <114255451+Brons1981@users.noreply.github.com>
2026-07-27 21:13:43 +0200

    feat: add discovery export module placeholder

---
+++ src/export.py
@@ -0,0 +1,5 @@
+"""Export functions for PicoT Discovery outputs.
+
+The neutral JSON discovery model will remain the source of truth. Excel and
+Markdown exporters will be added on top of that model.
+"""

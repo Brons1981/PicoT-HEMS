3a1849bfcb926c0a66d4f316dd279e7972e0044a
Brons1981 <114255451+Brons1981@users.noreply.github.com>
2026-08-13 16:27:28 +0200

    fix(v2): fail closed if legacy runtime is importable

--- picot_hems/run.sh
+++ picot_hems/run.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/with-contenv bashio
 set -e
 
+python - <<'PY'
+import importlib.util
+
+if importlib.util.find_spec("picot.addon") is not None:
+    raise SystemExit("Refusing to start PicoT v2: legacy picot.addon is importable")
+PY
+
 exec python -m picot.v2.live_runtime

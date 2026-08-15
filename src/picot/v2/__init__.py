d33db5ed8c9811602ad2f0d086629e7b7ff65653
Brons1981 <114255451+Brons1981@users.noreply.github.com>
2026-08-15 23:16:28 +0200

    release(v2): bump to 2.0.0-dev.37 (#245)
    * release(v2): bump runtime to dev.37
    
    * release(v2): align add-on with dev.37
    
    * release(v2): assert dev.37 version

--- src/picot/v2/__init__.py
+++ src/picot/v2/__init__.py
@@ -1,6 +1,6 @@
 """PicoT v2 canonical rebuild package."""
 
-__version__ = "2.0.0-dev.36"
+__version__ = "2.0.0-dev.37"
 
 ARCHITECTURE_BASELINE_COMMIT = "8197abbefd969f10da5a8f27244862be07998299"
 PIPELINE_CONTRACT_VERSION = 1

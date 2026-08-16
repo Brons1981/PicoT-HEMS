2960bd3403dba7ca1d494afd3605ad42b051fc81
Brons1981 <114255451+Brons1981@users.noreply.github.com>
2026-08-16 17:52:22 +0200

    release: 2.0.0-dev.77 (#332)
    * release: align picot_hems/config.yaml to 2.0.0-dev.77
    
    * release: align src/picot/v2/__init__.py to 2.0.0-dev.77
    
    * release: align tests/test_v2_version_alignment.py to 2.0.0-dev.77

--- src/picot/v2/__init__.py
+++ src/picot/v2/__init__.py
@@ -1,6 +1,6 @@
 """PicoT v2 canonical rebuild package."""
 
-__version__ = "2.0.0-dev.76"
+__version__ = "2.0.0-dev.77"
 
 ARCHITECTURE_BASELINE_COMMIT = "8197abbefd969f10da5a8f27244862be07998299"
 PIPELINE_CONTRACT_VERSION = 1
